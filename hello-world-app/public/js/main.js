d7bb156d423db8a4dc6de8c2ad2775202f6bfa33
xLitil <xlitil@DESKTOP-QEMDDA1.localdomain>
2018-11-30 15:23:12 +0100

    hello-world-app updated

--- hello-world-app/public/js/main.js
+++ hello-world-app/public/js/main.js
@@ -1,14 +1,19 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    hostname: ""
+    message: 'K8S Demo application',
+    hostname: "",
+    config: {}
   },
   created: function () {
     var vm = this;
     axios.get('/host')
     .then(function (response) {
             vm.hostname = response.data.hostname
+        })
+    axios.get('/config.json')
+    .then(function (response) {
+            vm.config = response.data
         })
   }
 })
